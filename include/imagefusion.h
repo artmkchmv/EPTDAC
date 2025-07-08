3758ab52ea23a2401afea5aa9c8efb831cb1efc6
artmkchmv <artemkakrut163@gmail.com>
2025-07-08 16:34:40 +0400

    Update imagefusion.h

--- include/imagefusion.h
+++ include/imagefusion.h
@@ -8,6 +8,9 @@ public:
     static cv::Mat fuseImagesEPTDAC(cv::Mat& TV_CPU_8U, cv::Mat& IR_CPU_8U,
                              const std::vector<cv::Point2f>& irPoints,
                              const std::vector<cv::Point2f>& tvPoints);
+    static cv::Mat fuseImagesEPTDAC_RGB(cv::Mat& TV_CPU_8U, cv::Mat& IR_CPU_8U,
+                                        const std::vector<cv::Point2f>& tvPoints,
+                                        const std::vector<cv::Point2f>& irPoints);
     static cv::Mat fuseImagesHalf(cv::Mat& TV_CPU_8U, cv::Mat& IR_CPU_8U);
     static cv::Mat fuseImagesMax(cv::Mat& TV_CPU_8U, cv::Mat& IR_CPU_8U);
     static cv::Mat fuseImagesByMask(cv::Mat& TV_CPU_8U, cv::Mat& IR_CPU_8U);
